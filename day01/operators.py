51e672b4cc9dc113e5d812995b92fdc6a8ff3443
Rafael-11 <raffypmir@gmail.com>
2024-06-11 22:13:33 -0400

    day01 op

---
+++ day01/operators.py
@@ -0,0 +1,31 @@
+# arithmetic: +, -, *, /,
+
+print(10-2)
+print(10+2)
+print(10 * 2)
+print(10 / 2) # in python, you will get always float or double number in division op
+print(10 % 3)
+
+print(100 / 2 ) # will get float
+print(int(100 / 2) ) # will get int
+
+# shorthand assignment operators: =, +=, -=, *=, /=, %=
+
+a=100
+a=200 # updated value of a
+
+print(a)
+
+a+=100
+
+print(a)
+
+a-=50
+print(a)
+
+# Division operator always give float even if divided even number
+a /=2
+print(a) # will give float
+print(int(a)) # will give int num
+
+
