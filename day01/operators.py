c56b479c64a6772b15399ada9c099012b4a9694e
Rafael-11 <raffypmir@gmail.com>
2024-06-17 12:11:54 -0400

    day01 logical operators

--- day01/operators.py
+++ day01/operators.py
@@ -28,4 +28,44 @@ a /=2
 print(a) # will give float
 print(int(a)) # will give int num
 
+# logical operators: &&, ||, ! --> Java
+# Python: and, or, not
+
+s='Hello world'
+print('H' and 'w' in s)
+
+print(True and True)
+print(True or False)
+print (False and True)
+
+s='Java Python C# c++'
+
+print('Java' or 'Ruby' in s)
+age=29
+citizen_ship='USA'
+is_elig= age >= 18 and citizen_ship=='USA'
+
+print(is_elig)
+
+# in or not in operator
+
+has_java='Java' in s
+print(has_java)
+# in Java ! contains() but in Python will use 'not in'
+print('Python' not in s)
+
+print( not True)
+print(not False) # same ! in java, ex: !true
+
+# identity operator in Python
+
+print('-------------------------------')
+# It is pointing the same object
+s='Java'
+s2='Java'
+
+print(s is s2) # == in Java, checking two variable if it is referring to same objects.
+
+
+
 
