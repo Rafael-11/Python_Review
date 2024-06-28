bbf578a2d754f44c3d505d869f18dc32087180d4
rafael <raffypamir@gmail.com>
2024-06-27 19:37:49 -0500

    day02 string

---
+++ day02/string.py
@@ -0,0 +1,45 @@
+name = 'python'
+
+# length
+print(len(name))
+
+# access any element
+print(name[0])
+
+# last index
+print(name[-1])
+print(name[-2])
+
+# slicing string is same substring in Java
+
+s = 'Java Programming language'
+s2=s[5:16]
+print(s2)
+
+s3=s[:4]
+print(s3)
+
+s4=s[:-1]
+print(s4)
+
+s5=s[17:-1]
+print(s5)
+
+# reverse the string after slicing
+s6=s[::-1]
+print(s6)
+
+# reverse string in python with function
+s7 = 'Python Programming'
+reversed(s7)
+print(s7)
+
+# slicing from starting
+
+s8 = 'Java is fun'
+s9=s8[8:]
+print(s9)
+
+# get help with str doc
+print(help(str))
+
